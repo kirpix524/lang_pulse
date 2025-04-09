a0306a838927dec6579682f9773a2c4b7754d2e0
Николай <kirpix524@gmail.com>
2025-04-09 17:21:24 +0400

    added saving user to users.txt and loading user list

--- storage/db.py
+++ storage/db.py
@@ -1,40 +1,32 @@
-import json
-import os
-
-from docutils.parsers.rst.directives.misc import Class
-
 from models.user import User
 from models.word import Word
+
+import storage.config as config
 
 class DataBase:
     def __init__(self):
         pass
 
-    def save_user(self, user: User):
+    def save_user_list(self, user_list: list[User]) -> None:
         pass
 
-    def save_user_list(self, user_list: []):
+    def save_word(self, word: Word, user: User) -> None:
         pass
 
-    def save_word(self, word: Word):
+    def load_user_list(self) -> list[User]:
         pass
 
-    def load_user_list(self):
+    def load_word_list(self, user:User) -> list[Word]:
         pass
 
-    def load_word_list(self, user:User):
-        pass
-
-    def load_user(self, user:User):
-        pass
 
 class DBFile(DataBase):
     def __init__(self, file_name):
         super().__init__()
         self.file_name = file_name
 
-    def load_user_list(self):
-        users = []
+    def load_user_list(self) -> list[User]:
+        users: list[User] = []
         try:
             with open(self.file_name, "r", encoding="utf-8") as file:
                 for line in file:
@@ -42,13 +34,17 @@ class DBFile(DataBase):
                     if not line or "|" not in line:
                         continue
                     user_id, username = line.split("|", 1)
-                    users[username] = User(user_id.strip(), username.strip())
+                    user = User(username.strip(), int(user_id.strip()))
+                    users.append(user)
         except FileNotFoundError:
             print(f"Файл {self.file_name} не найден. Будет создан при сохранении.")
 
-    def save_user_list(self, user_list):
-        with open(self.file_name, "w", encoding="utf-8") as file:
-            for user in user_list.values():
-                file.write(f"{user.id}|{user.username}\n")
+        return users
 
+    def save_user_list(self, user_list: list[User]) -> None:
+        with open(self.file_name, "w", encoding="utf-8") as file:
+            for user in user_list:
+                file.write(f"{user.userid}|{user.username}\n")
+
+db = DBFile(config.USERS_DB_FILE)
 
--- storage/user_repo.py
+++ storage/user_repo.py
@@ -1,13 +1,20 @@
+from storage.db import db
+from models.user import User
 # Временная заглушка вместо настоящего хранилища пользователей
 class UserRepository:
     def __init__(self):
-        self.users = {}
+        self.users: list[User] = db.load_user_list()
+
+    def __get_new_user_id(self):
+        return len(self.users)
 
     def add_user(self, username):
-        self.users[username] = {}
+        user = User(username, self.__get_new_user_id())
+        self.users.append(user)
+        db.save_user_list(self.users)
 
-    def get_usernames(self):
-        return list(self.users.keys())
+    def get_usernames(self) -> list[str]:
+        return [user.username for user in self.users]
 
-    def user_exists(self, username):
-        return username in self.users
+    def user_exists(self, username: str) -> bool:
+        return any(user.username == username for user in self.users)
