6433b2fa16f883fa3757fdab1b2e5c1df71428c3
Николай <kirpix524@gmail.com>
2025-04-09 19:05:06 +0400

    main menu stile is loaded from .kv file

--- storage/config.py
+++ storage/config.py
@@ -6,3 +6,13 @@ with open(CONFIG_PATH, "r", encoding="utf-8") as config_file:
 
 USERS_DB_FILE = config["users_db_file"]
 LOGS_DIRECTORY = config["logs_directory"]
+SCREEN_WIDTH = config["screen_settings"]["width"]
+SCREEN_HEIGHT = config["screen_settings"]["height"]
+LOGIN_SCREEN_CHOOSE_USER_TEXT = config["login_screen_settings"]["choose_user_spinner_text"]
+LOGIN_SCREEN_LOGIN_BUTTON_TEXT = config["login_screen_settings"]["login_button_text"]
+LOGIN_SCREEN_REGISTER_BUTTON_TEXT = config["login_screen_settings"]["register_button_text"]
+REGISTER_SCREEN_USER_NAME_INPUT_HINT_TEXT = config["register_screen_settings"]["user_name_input_hint_text"]
+REGISTER_SCREEN_SUBMIT_BUTTON_TEXT = config["register_screen_settings"]["submit_button_text"]
+MAIN_MENU_SCREEN_MAIN_MENU_LABEL_TEXT = config["main_menu_screen_settings"]["main_menu_label_text"]
+MAIN_MENU_SCREEN_SHOW_DICTIONARY_BUTTON_TEXT = config["main_menu_screen_settings"]["show_dictionary_button_text"]
+MAIN_MENU_SCREEN_SHOW_TRAINING_BUTTON_TEXT = config["main_menu_screen_settings"]["show_training_button_text"]
--- ui/gui.py
+++ ui/gui.py
@@ -1,11 +1,14 @@
+from kivy.core.window import Window
 from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.spinner import Spinner
 from kivy.uix.screenmanager import ScreenManager, Screen
-from storage.db import DBFile
+import storage.config as config
 from storage.user_repo import UserRepository
 
 # Экран выбора пользователя
@@ -15,9 +18,9 @@ class LoginScreen(Screen):
         self.user_repo = user_repo
         self.layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
 
-        self.spinner = Spinner(text='Выберите пользователя', values=self.user_repo.get_usernames())
-        self.login_button = Button(text='Войти')
-        self.register_button = Button(text='Зарегистрироваться')
+        self.spinner = Spinner(text=config.LOGIN_SCREEN_CHOOSE_USER_TEXT, values=self.user_repo.get_usernames())
+        self.login_button = Button(text=config.LOGIN_SCREEN_LOGIN_BUTTON_TEXT)
+        self.register_button = Button(text=config.LOGIN_SCREEN_REGISTER_BUTTON_TEXT)
 
         self.layout.add_widget(self.spinner)
         self.layout.add_widget(self.login_button)
@@ -44,8 +47,8 @@ class RegisterScreen(Screen):
         self.user_repo = user_repo
         self.layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
 
-        self.input = TextInput(hint_text='Введите имя пользователя', multiline=False)
-        self.submit_button = Button(text='Создать и войти')
+        self.input = TextInput(hint_text=config.REGISTER_SCREEN_USER_NAME_INPUT_HINT_TEXT, multiline=False)
+        self.submit_button = Button(text=config.LOGIN_SCREEN_LOGIN_BUTTON_TEXT)
 
         self.layout.add_widget(self.input)
         self.layout.add_widget(self.submit_button)
@@ -66,11 +69,14 @@ class RegisterScreen(Screen):
 class MainMenuScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
-        layout.add_widget(Label(text='Главное меню'))
-        layout.add_widget(Button(text='Показать словарь'))
-        layout.add_widget(Button(text='Показать тренировки'))
-        self.add_widget(layout)
+        self.menu_title = StringProperty(config.MAIN_MENU_SCREEN_MAIN_MENU_LABEL_TEXT)
+        self.show_dictionary_text = StringProperty(config.MAIN_MENU_SCREEN_SHOW_DICTIONARY_BUTTON_TEXT)
+        self.show_training_text = StringProperty(config.MAIN_MENU_SCREEN_SHOW_TRAINING_BUTTON_TEXT)
+        # layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
+        # layout.add_widget(Label(text=config.MAIN_MENU_SCREEN_MAIN_MENU_LABEL_TEXT))
+        # layout.add_widget(Button(text=config.MAIN_MENU_SCREEN_SHOW_DICTIONARY_BUTTON_TEXT))
+        # layout.add_widget(Button(text=config.MAIN_MENU_SCREEN_SHOW_TRAINING_BUTTON_TEXT))
+        # self.add_widget(layout)
 
 # Менеджер экранов
 class LangPulseApp(App):
@@ -78,9 +84,11 @@ class LangPulseApp(App):
         self.user_repo = UserRepository()
         self.sm = ScreenManager()
         self.sm.current_user = None
+        Builder.load_file("styles/main_menu.kv")
 
         self.sm.add_widget(LoginScreen(name='login', user_repo=self.user_repo))
         self.sm.add_widget(RegisterScreen(name='register', user_repo=self.user_repo))
         self.sm.add_widget(MainMenuScreen(name='main_menu'))
 
+        Window.size = (config.SCREEN_WIDTH, config.SCREEN_HEIGHT)
         return self.sm
